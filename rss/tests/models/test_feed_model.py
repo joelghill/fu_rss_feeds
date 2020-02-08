e863e14936a242a6088d12a8db5d85299ee944d6
Joel Hill <joel.hill.87@gmail.com>
2020-02-08 13:19:01 -0600

    Tweaking settings and test

--- rss/tests/models/test_feed_model.py
+++ rss/tests/models/test_feed_model.py
@@ -1,9 +1,15 @@
+""" Module containing tests for the feed model
+"""
 from django.test import TestCase
-
 from rss.models.feed import Feed
 
 
 class InitFeedTestCase(TestCase):
+    """ Basic initialization test case
+    """
 
     def test_init_feed(self):
+        """ Create a feed instance and verify correct initialization
+        """
         feed = Feed.objects.create()
+        self.assertIsNotNone(feed)
