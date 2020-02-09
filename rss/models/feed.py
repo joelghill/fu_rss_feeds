7621e6289503fdea2428c1e24e9eb202e917405e
Joel Hill <joel.hill.87@gmail.com>
2020-02-08 20:09:45 -0600

    Fix to feeds models

--- rss/models/feed.py
+++ rss/models/feed.py
@@ -10,7 +10,7 @@ class Feed(models.Model):
     url = models.URLField(max_length=200, blank=False, null=False)
     display_name = models.CharField(max_length=200, name='Name', blank=True)
     description = models.TextField(name='Description', blank=True)
-    e_tag = models.CharField(name='ETag', blank=True, null=True)
+    e_tag = models.CharField(max_length=200, name='ETag', blank=True, null=True)
     last_modified = models.DateTimeField(name='Last Modified', blank=True, null=True)
 
     raw_feed = models.TextField
