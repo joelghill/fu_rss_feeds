b58cc4a5940c5613a783c292eca396409bf254df
Joel Hill <joel.hill.87@gmail.com>
2020-02-08 20:38:15 -0600

    Registering models for admin site

--- web_presence/admin.py
+++ web_presence/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from web_presence.models import SocialMediaType, SocialMediaEndpoint, WebIdentity
 
 # Register your models here.
+admin.site.register(SocialMediaEndpoint)
+admin.site.register(SocialMediaType)
+admin.site.register(WebIdentity)
