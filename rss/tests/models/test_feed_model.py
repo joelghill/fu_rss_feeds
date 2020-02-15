0eac621133d887f78ca72c67341753e1d84ad7f0
Joel Hill <joel.hill.87@gmail.com>
2020-02-15 11:23:54 -0600

    Addition of test fixtures and code

--- rss/tests/models/test_feed_model.py
+++ rss/tests/models/test_feed_model.py
@@ -11,5 +11,19 @@ class InitFeedTestCase(TestCase):
     def test_init_feed(self):
         """ Create a feed instance and verify correct initialization
         """
-        feed = Feed.objects.create(url='https://www.xkcd.com/rss.xml')
+        feed = Feed.objects.create(source='rss/tests/fixtures/xkcd_feed.xml')
         self.assertIsNotNone(feed)
+
+    def test_first_time_update_feed(self):
+        """ Create a feed instance and verify correct initialization
+        """
+        feed = Feed.objects.create(source='https://www.penny-arcade.com/feed')
+        self.assertIsNotNone(feed)
+        feed.update()
+
+    def test_fu_politics_update_feed(self):
+        """ Create a feed instance and verify correct initialization
+        """
+        feed = Feed.objects.create(source='https://feed.podbean.com/fupolitics/feed.xml')
+        self.assertIsNotNone(feed)
+        feed.update()
